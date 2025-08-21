21f9af7227cfb8a2af24135ec3d0fd0ef1927684
mlwx <44472545+mlwx@users.noreply.github.com>
2025-08-21 19:52:47 +0800

    Create VotingBooth.sol

---
+++ VotingBooth.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract VotingBooth{
+
+    struct Proposal {
+        bytes32 name;     // Proposal name
+        uint voteCount;   // Number of accumulated votes
+    }
+
+    struct Voter {
+        bool voted;
+        uint weight;
+        uint vote;
+    }
+
+    address public leader;
+    mapping (address => Voter) public voters;
+    Proposal[] public proposals;
+
+    constructor(bytes32[] memory proposalNames){
+        leader = msg.sender;
+        voters[leader].weight = 1;
+
+        for (uint i = 0; i < proposalNames.length; i++){
+            proposals.push(Proposal(proposalNames[i],0));
+        }
+    }
+
+    function allowVotingRights(address voter) external {
+        require(msg.sender == leader, "No permissions");
+        require(voters[voter].voted != true, "Already voted");
+        require(voters[voter].weight == 0 , "Already has rights");
+        voters[voter].weight = 1;
+    }
+
+    function bytes32ToString(bytes32 _bytes32) public pure returns (string memory) {
+        uint8 length = 0;
+        // Count length until null byte
+        while(length < 32 && _bytes32[length] != 0) {
+            length++;
+        }
+
+        bytes memory bytesArray = new bytes(length);
+        for (uint8 i = 0; i < length; i++) {
+            bytesArray[i] = _bytes32[i];
+        }
+        return string(bytesArray);
+    }
+    
+    function displayProposals() external view returns (string[] memory){
+        string[] memory names = new string[](proposals.length);
+        for (uint i = 0; i < proposals.length; i++){
+            names[i] = bytes32ToString(proposals[i].name); 
+        }
+        return names;
+    }
+
+    function displayVoteCount() external view returns (uint[] memory){
+        uint[] memory count = new uint[](proposals.length);
+        for (uint i = 0; i < proposals.length; i++){
+            count[i] = proposals[i].voteCount;
+        }
+        return count;
+    }
+
+    function vote(uint proposal) external{
+        require(voters[msg.sender].voted == false, "Already voted");
+        require(voters[msg.sender].weight == 1, "No voting rights");
+        proposals[proposal].voteCount += 1;
+        voters[msg.sender].voted = true;
+    }
+
+    function getWinningProposal() public view returns (uint winningProposal_){
+        uint winningVoteCount = 0;
+        for (uint i = 0; i < proposals.length; i++){
+            if (proposals[i].voteCount > winningVoteCount){
+                winningVoteCount = proposals[i].voteCount;
+                winningProposal_ = i;
+            }
+        }
+    }
+
+    function declareWinningProposal() external view returns (string memory winnerName_, uint voteCount_){
+        uint winningIndex = getWinningProposal();
+        winnerName_ = bytes32ToString(proposals[winningIndex].name);
+        voteCount_ = proposals[winningIndex].voteCount;
+    }        
+}
